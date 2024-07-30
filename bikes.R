53e5c3cf50fc249ae8cc66a9507b370ef56deeb8
ibrahimemam7 <1ibrahimemam@gmail.com>
2024-07-29 23:34:45 -0400

    began EDA

--- bikes.R
+++ bikes.R
@@ -2,17 +2,47 @@
 #' Ibrahim Emam
 #' July/29/2024
 
+#import relevant libraries
+library(tidyverse)
+library(funModel)
+library(lubridate)
+
 # start by importing the csv files into data frames
 station <- read.csv("raw_data/station.csv")
 trip <- read.csv("raw_data/trip.csv")
 weather <- read.csv("raw_data/weather.csv")
 
-# get an idea of what information is contained within each data frame
+# get an idea of what information is contained in the station dataframe
 head(station)
+
+# determine the range of the values to identify potential outliers
+# summary also shows the type for each variable
 summary(station)
 
+# identify zeros or NAs
+status(station)
+
+# visualize distributions for categorical variables
+freq(station)
+
+# repeat similar steps for the trip data frame
 head(trip)
-summary(station)
+summary(trip)
+status(trip)
+freq(trip)
 
+#' visualize the distribution of numerical variables in the trips data frame
+#' did not do this for station dataframe because there were no important numerical
+#' variables (i.e. showing distribution of longitude/latitude is not helpful)
+plot_num(trip)
+
+#' it seems the previous histogram does not show the distribution of the data for
+#' trip duration, likely because the range of the data is very wide. Use log.
+hist(log(trip$duration))
+
+# repeat similar steps for the weather data frame
 head(weather)
 summary(weather)
+status(weather)
+freq(weather)
+plot_num(weather)
